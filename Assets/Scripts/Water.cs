217984f12499d4e591d9cc5a9df8e4c73332051e
Lucas <l.deutschmann@rubika-edu.com>
2021-04-23 17:04:38 +0200

    almoooost

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Water : MonoBehaviour
 {
+    int roomSize = 10;
     public float moveSpeed = 5f;
     public Transform movePoint;
 
@@ -12,6 +13,7 @@ public class Water : MonoBehaviour
     public Collider2D currentEllement;
     RotateLevel rotateRef;
     float verticalRange = 0f;
+    bool checkedSides;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +28,11 @@ public class Water : MonoBehaviour
     {
         if (!rotateRef.rotate)
         {
+            if (transform.position == movePoint.position && !checkedSides)
+            {
+                CheckSides();
+                checkedSides = true;
+            }
             transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
             currentEllement = Physics2D.OverlapCircle(movePoint.position + new Vector3(0, verticalRange, 0f), .2f, floatables);
 
@@ -33,12 +40,12 @@ public class Water : MonoBehaviour
             {
                 if (currentEllement != null)
                 {
-                    
+
                     if (currentEllement.GetComponent<Movement>() != null)
                     {
                         if (!currentEllement.GetComponent<Movement>().floating)
                         {
-                            
+
                             currentEllement.GetComponent<Movement>().floating = true;
                             currentEllement.GetComponent<Movement>().movePoint.position += new Vector3(0f, 1f, 0f);
                         }
@@ -46,20 +53,33 @@ public class Water : MonoBehaviour
                     }
                     if (currentEllement.GetComponent<Wood>() != null)
                     {
-
+                        //do smth
                     }
                 }
             }
             if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1, 0f), .2f, delimitations) && Mathf.Abs(movePoint.position.x - transform.position.x) <= .05f)
             {
-                
+
                 movePoint.position += new Vector3(0f, -1, 0f);
+                checkedSides = false;
             }
         }
+    }
 
-
-
-
-
+    void CheckSides()
+    {
+        for (int i = 0; i < roomSize; i++)
+        {
+            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(i, 0, 0f), .2f, delimitations))
+            {
+                transform.localScale += new Vector3(.85f, 0, 0);
+                transform.position += new Vector3(.5f, 0, 0);
+            }
+            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(-i, 0, 0f), .2f, delimitations))
+            {
+                transform.localScale += new Vector3(.85f, 0, 0);
+                transform.position += new Vector3(.5f, 0, 0);
+            }
+        }
     }
 }
